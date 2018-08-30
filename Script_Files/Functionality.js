6086a575f22e676f3e7bfab909f90b49655e9436
Bhabaranjan19966 <bhabaranjanpanigrahi@gmail.com>
2018-08-30 09:54:55 +0530

    Sript tags moved to the end of body

--- Script_Files/Functionality.js
+++ Script_Files/Functionality.js
@@ -23,16 +23,16 @@ $(document).ready(function () {
     });
 });
 
-theEnableFunction = (obj) =>{
-    var el=document.getElementById(obj);
-    var k =""+el.style.display;
-    if(k=='block'||k=='flex'){
-        el.style.display='none';
+theEnableFunction = (obj) => {
+    var el = document.getElementById(obj);
+    var k = "" + el.style.display;
+    if (k == 'block' || k == 'flex') {
+        el.style.display = 'none';
         return;
     }
-    else{
-        window.scrollTo(0 , 0);
-        el.style.display='flex';
+    else {
+        window.scrollTo(0, 0);
+        el.style.display = 'flex';
         return;
     }
 }
